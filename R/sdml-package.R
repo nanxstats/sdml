2d6456b7cd2e2ac1b2ecf67734b5e163089009f6
Xiao Nan <road2stat@gmail.com>
2012-01-07 03:54:49 +0800

    added the sdml-package help page

---
+++ R/sdml-package.R
@@ -0,0 +1,32 @@
+#' Supervised Distance Metric Learning with R
+#'
+#' The sdml package aims to implement the state-of-the-art 
+#' algorithms for supervised distance metric learning.
+#' These methods are widely applied in clustering, 
+#' classification, information retrieval, 
+#' and computer vision problems.
+#'
+#' \tabular{ll}{ Package: \tab sdml\cr Type: \tab Package\cr
+#' Version: \tab 0.1\cr License: \tab BSD\cr } Additional Desc.
+#'
+#' @section Relevant Component Analysis: Put RCA desc. here.
+#'
+#' @section Local Fisher Discriminant Analysis: Put LFDA desc. here.
+#'
+#' @name sdml-package
+#' @aliases sdml
+#' @docType package
+#' @exportPattern "^[^\\.]"
+#' @author Xiao Nan <\url{http://www.road2stat.com}>
+#'           Gao Tao <\url{http://www.gaotao.name}>
+#' @note Bug reports and feature requests could be sent to
+#' \url{https://github.com/road2stat/sdml/issues}.
+#' @references The associated website for this package:
+#' \url{https://github.com/road2stat/sdml/}
+#'
+#' @seealso Add \code{\link{rca}}, \code{\link{lfda}}, etc.
+#' 
+#' @keywords package supervised distance metric learning 
+#' @example add package examples in inst/examples/sdml-package-ex.R
+NULL
+
