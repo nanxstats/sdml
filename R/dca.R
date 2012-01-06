d11b2294793de0fe1c2ad90122decc40189686b3
Xiao Nan <road2stat@gmail.com>
2012-01-07 01:53:03 +0800

    initialize dca functions

---
+++ R/dca.R
@@ -0,0 +1,39 @@
+#' Discriminative Component Analysis
+#'
+#' Performs discriminative component analysis on the given data.
+#' 
+#' Put DCA function details here.
+#' 
+#' @param 
+#' @param 
+#' @param 
+#' @param 
+#' 
+#' @return list of the DCA results:
+#' \item{}{}
+#' \item{}{}
+#' \item{}{}
+#' 
+#' @keywords dca discriminant component transformation mahalanobis metric
+#'
+#' @note Put some note here.
+#' 
+#' @author Xiao Nan \email{road2stat@gmail.com}
+#' 
+#' @seealso See \code{\link{dcaConstrains}} and
+#'          \code{\link{dcaConstrainsNum}} for generating contextual constraints.
+#'
+#' @export dca
+#' 
+#' @references 
+#'
+#' @examples
+#' dca(letters)
+#' dca(c("i", "like", "programming", NA))
+
+dca <- function() {
+
+
+
+}
+
