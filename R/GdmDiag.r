fd14ce16f60866b787204564a73e885a2004c096
Tao Gao <joegaotao@gmail.com>
2012-05-12 13:48:14 +0800

    global distance metric learning

--- R/GdmDiag.r
+++ R/GdmDiag.r
@@ -1,8 +1,8 @@
 #' Global Distance Metric Learning
 #'
-#' Performs Global Distance Metric Learning (GDM) on the given data.
+#' Performs Global Distance Metric Learning (GDM) on the given data, learning a diagonal matrix.
 #' 
-#' Put GDM function details here.
+#' Put GdmDiag function details here.
 #' 
 #' @param data \code{n * d} data matrix. \code{n} is the number of data points,
 #'             \code{d} is the dimension of the data.
@@ -12,22 +12,24 @@
 #'				For example, pair(1, 3) represents the first observation is similar the 3th observation in the original data.
 #' @param dism \code{n * 2} matrix describing the dissimilar constrains as \code{simi}.
 #'				Each row of matrix is serial number of a dissimilar pair in the original data.
+#' @param C0 numeric, the bound of similar constrains.
+#' @param threshold numeric, the threshold of stoping the learning iteration.
 #' 
-#' @return list of the DCA results:
-#' \item{diagonalA}{GDM suggested Mahalanobis matrix}
+#' @return list of the GdmDiag results:
+#' \item{newData}{GdmDiag transformed data}
+#' \item{diagonalA}{suggested Mahalanobis matrix}
+#' \item{dmlA}{matrix to transform data, square root of diagonalA }
 #' \item{error}{the precision of obtained distance metric by Newton-Raphson optimization }
 #' 
 #' For every two original data points (x1, x2) in newData (y1, y2):
 #' 
-#' \eqn{(x2 - x1)' * B * (x2 - x1) = || (x2 - x1) * A ||^2 = || y2 - y1 ||^2} 
+#' \eqn{(x2 - x1)' * A * (x2 - x1) = || (x2 - x1) * B ||^2 = || y2 - y1 ||^2} 
 #' 
 #' @keywords GDM global distance metirc learning transformation mahalanobis metric
 #' 
 #' @note Be sure to check whether the dimension of original data and constrains' format are valid for the function.
 #' 
 #' @author Gao Tao <\url{http://www.gaotao.name}>
-#' 
-#' @export DiagA
 #' 
 #' @references
 #' Steven C.H. Hoi, W. Liu, M.R. Lyu and W.Y. Ma (2003).
@@ -56,24 +58,27 @@
 #' # define disimilar constrains
 #' dism <- t(as.matrix(tol[!tol %in% simi]))
 #'
-#' # transform data using GDM 
-#' result <- DiagA(data, simi, dism)
-#' newData <- data %*% sqrt(result[[1]])
-#'
+#' # transform data using GdmDiag 
+#' result <- GdmDiag(data, simi, dism)
+#' newData <- result$newData
 #' # plot original data
 #' color <- gl(2, k, labels = c("red", "blue"))
 #' par(mfrow = c(2, 1), mar = rep(0, 4) + 0.1)
 #' scatterplot3d(data, color = color, cex.symbols = 0.6, 
 #'			  xlim = range(data[, 1], newData[, 1]), 
 #'			  ylim = range(data[, 2], newData[, 2]),
-#'			  zlim = range(data[, 3], newData[, 3]))
-#' # plot GDM transformed data
+#'			  zlim = range(data[, 3], newData[, 3])
+#'			  main = "Original Data")
+#' # plot GdmDiag transformed data
 #' scatterplot3d(newData, color = color, cex.symbols = 0.6, 
 #'			  xlim = range(data[, 1], newData[, 1]), 
 #'			  ylim = range(data[, 2], newData[, 2]),
-#'			  zlim = range(data[, 3], newData[, 3]))
-			  
-DiagA <- function(data, simi, dism, C0 = 1, S1 = NULL, D1 = NULL, fudge = 0.000001, threshold = 0.001, reduction = 2) {
+#'			  zlim = range(data[, 3], newData[, 3])
+#'			  main = "Transformed Data")
+		  
+GdmDiag <- function(data, simi, dism, C0 = 1, S1 = NULL, D1 = NULL, threshold = 0.001) {
+		fudge = 0.000001
+		reduction = 2
 		data <- as.matrix(data)
 		simi <- as.matrix(simi)
 		dism <- as.matrix(dism)
@@ -133,7 +138,7 @@ DiagA <- function(data, simi, dism, C0 = 1, S1 = NULL, D1 = NULL, fudge = 0.0000
 
 			fDo <- log(sum(sqrt((dist1.dism^2) %*% atemp)))
 			obj <- as.numeric(s.sum %*% atemp) + C0 * fDo
-			obj.previous = obj * 1.1
+			obj.previous <- obj * 1.1
 		
 			while (obj < obj.previous) {
 				lambda.previous <- lambda
@@ -146,8 +151,11 @@ DiagA <- function(data, simi, dism, C0 = 1, S1 = NULL, D1 = NULL, fudge = 0.0000
 				obj <- as.numeric(s.sum %*% atemp) + C0 * fDo1 
 			}
 		a <- a.previous
-		error = abs((obj.previous - obj.initial)/obj.previous)
+		error <- abs((obj.previous - obj.initial)/obj.previous)
 		}
-		return(list("diagonalA " = diag(as.numeric(a)), "error" = error))
+		diagnoalA <- diag(as.numeric(a))
+		dmlA <- sqrt(diagonalA)
+		newData <- data %*% dmlA
+		
+		return(list("newData" = newData, "diagonalA" = diagonalA, "dmlA" = dmlA, "error" = error))
 }
-
